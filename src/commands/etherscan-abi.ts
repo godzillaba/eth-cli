7d5c36890b8cbda0292d8cc0c1fdd165c59dd7db
Henry <11198460+godzillaba@users.noreply.github.com>
2024-12-13 10:57:37 -0500

    proxy

--- src/commands/etherscan-abi.ts
+++ src/commands/etherscan-abi.ts
@@ -2,28 +2,19 @@ import { Command } from '@commander-js/extra-typings';
 import { log } from '../utils/logger.ts';
 import { providers } from 'ethers';
 import { getAbi } from '../utils/etherscan.ts';
+import { assertDefined, getProxyAddress } from '../utils/misc.ts';
 
 export function etherscanAbiCommand(program: Command) {
   program
     .command('etherscan-abi')
     .description('Get ABI a contract')
     .argument('<address>', 'Ccontract address')
-    .option('-c, --chain <CHAIN>', 'Chain ID')
+    .option('-p, --proxy', 'Get the proxy implementation ABI')
     .option('-r, --rpc <RPC>', 'RPC URL')
     .action(async (addr: string, options) => {
-      if (!options.chain && !options.rpc) {
-        log.error('Please specify --chain or --rpc');
-        process.exit(1);
-      }
-
-      let chainId: number;
-      if (options.chain) {
-        chainId = parseInt(options.chain);
-      } else {
-        const provider = new providers.JsonRpcProvider(options.rpc);
-        chainId = (await provider.getNetwork()).chainId;
-      }
-
+      const provider = new providers.JsonRpcProvider(assertDefined(options.rpc, 'Please specify --rpc'));
+      const chainId = (await provider.getNetwork()).chainId;
+      addr = options.proxy ? await getProxyAddress(provider, addr) : addr;
       log.info(await getAbi(chainId, addr));
     });
 }
--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,4 +1,7 @@
+import type { providers } from 'ethers';
 import { log } from './logger';
+
+const PROXY_IMPL_SLOT = '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc';
 
 export function assertDefined<T>(value: T | undefined | null, message: string): T {
   if (value === undefined || value === null) {
@@ -6,4 +9,9 @@ export function assertDefined<T>(value: T | undefined | null, message: string): 
     process.exit(1);
   }
   return value;
+}
+
+export async function getProxyAddress(provider: providers.JsonRpcProvider, addr: string) {
+  const data = await provider.getStorageAt(addr, PROXY_IMPL_SLOT);
+  return '0x' + data.slice(26);
 }
