164375b92489803ddfb8f164ab071f9655721e62
Henry <11198460+godzillaba@users.noreply.github.com>
2024-12-13 10:48:38 -0500

    log

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,7 @@
 import chalk from 'chalk';
 
 export const log = {
-  info: (message: string) => console.log(chalk.white(message)),
+  info: (message: string) => console.log(message),
   success: (message: string) => console.log(chalk.greenBright(message)),
   warning: (message: string) => console.log(chalk.yellowBright(message)),
   error: (message: string) => console.log(chalk.redBright(message))
