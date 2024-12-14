15085331bbbb6fd024b8421c2f84d67523aff2fd
Henry <11198460+godzillaba@users.noreply.github.com>
2024-12-14 16:03:09 -0500

    pretty

--- src/commands/verify-bytecode.ts
+++ src/commands/verify-bytecode.ts
@@ -2,7 +2,11 @@ import { Command } from '@commander-js/extra-typings'
 import { log } from '../utils/logger.ts'
 import { providers } from 'ethers'
 import { getContractCreation } from '../utils/etherscan.ts'
-import { defaultAbiCoder, getAddress, getContractAddress } from 'ethers/lib/utils'
+import {
+  defaultAbiCoder,
+  getAddress,
+  getContractAddress,
+} from 'ethers/lib/utils'
 import { getProxyImpl } from '../utils/misc.ts'
 
 function getContractName(artifact: any) {
@@ -31,7 +35,10 @@ export function verifyBytecodeCommand(program: Command) {
     .argument('<ADDRESS>', 'Ccontract address')
     .argument('<ARTIFACT_FILE>', 'Path to the build artifact file')
     .option('-r, --rpc <RPC>', 'RPC URL')
-    .option('-p, --proxy', 'Verify the proxy implementation bytecode. Will not verify the proxy itself')
+    .option(
+      '-p, --proxy',
+      'Verify the proxy implementation bytecode. Will not verify the proxy itself'
+    )
     .action(async (addr: string, artifactFile: string, options) => {
       const provider = new providers.JsonRpcProvider(options.rpc)
 
@@ -41,25 +48,25 @@ export function verifyBytecodeCommand(program: Command) {
       addr = getAddress(addr)
 
       if (options.proxy) {
-        const proxyAddr = addr;
+        const proxyAddr = addr
         addr = await getProxyImpl(provider, addr)
         log.info(`Proxy Address: ${proxyAddr}`)
         log.info(`Implementation Address: ${addr}`)
-      }
-      else {
+      } else {
         log.info(`Contract Address: ${addr}`)
       }
-      
+
       // steps:
       // - get the creation tx
       // - make sure it is a creation tx
       // - make sure the resulting contract address is the same as the one we are verifying
       // - make sure the artifact bytecode is a prefix of the tx input data
       // - decode the constructor arguments and display them
-      
+
       // get the creation tx
       const chainId = (await provider.getNetwork()).chainId
-      const creationTxHash = (await getContractCreation(chainId, [addr]))[0].txHash;
+      const creationTxHash = (await getContractCreation(chainId, [addr]))[0]
+        .txHash
       if (!creationTxHash) {
         throw new Error('Creation transaction not found')
       }
@@ -73,7 +80,9 @@ export function verifyBytecodeCommand(program: Command) {
 
       // make sure the resulting contract address is the same as the one we are verifying
       if (getContractAddress(tx) !== addr) {
-        throw new Error(`Contract address mismatch. Expected ${addr}, got ${getContractAddress(tx)}`)
+        throw new Error(
+          `Contract address mismatch. Expected ${addr}, got ${getContractAddress(tx)}`
+        )
       }
 
       // make sure the artifact bytecode is a prefix of the tx input data
@@ -84,11 +93,18 @@ export function verifyBytecodeCommand(program: Command) {
       }
 
       // decode the constructor arguments and display them
-      const constructorAbi = artifact.abi.find((abi: any) => abi.type === 'constructor');
-      const decoded = defaultAbiCoder.decode(constructorAbi.inputs, '0x' + tx.data.slice(artifactBytecode.length))
+      const constructorAbi = artifact.abi.find(
+        (abi: any) => abi.type === 'constructor'
+      )
+      const decoded = defaultAbiCoder.decode(
+        constructorAbi.inputs,
+        '0x' + tx.data.slice(artifactBytecode.length)
+      )
       log.info('Constructor arguments:')
       for (let i = 0; i < constructorAbi.inputs.length; i++) {
-        log.info(`${constructorAbi.inputs[i].type} ${constructorAbi.inputs[i].name}:\n  ${decoded[i]}`)
+        log.info(
+          `${constructorAbi.inputs[i].type} ${constructorAbi.inputs[i].name}:\n  ${decoded[i]}`
+        )
       }
 
       log.success('Bytecode verified')
