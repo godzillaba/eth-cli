2f82426ead41ba314f2f26d4f4f78bf58f2127da
Henry <11198460+godzillaba@users.noreply.github.com>
2025-01-07 14:10:49 -0500

    fmt

--- src/commands/get-fee-collectors.ts
+++ src/commands/get-fee-collectors.ts
@@ -1,7 +1,12 @@
 import { Command } from '@commander-js/extra-typings'
 import { log } from '../utils/logger'
 import { providers } from 'ethers'
-import { getBatchPosters, getL1BaseFeeCollector, getL1SurplusFeeCollector, getL2BaseFeeCollector } from '../utils/misc'
+import {
+  getBatchPosters,
+  getL1BaseFeeCollector,
+  getL1SurplusFeeCollector,
+  getL2BaseFeeCollector,
+} from '../utils/misc'
 
 export function getFeeCollectorsCommand(program: Command) {
   program
@@ -12,7 +17,9 @@ export function getFeeCollectorsCommand(program: Command) {
       const provider = new providers.JsonRpcProvider(options.rpc)
       const tab = '  '
       const posters = await getBatchPosters(provider)
-      const l1BaseCollectors = await Promise.all(posters.map(poster => getL1BaseFeeCollector(provider, poster)))
+      const l1BaseCollectors = await Promise.all(
+        posters.map(poster => getL1BaseFeeCollector(provider, poster))
+      )
       const l1SurplusCollector = await getL1SurplusFeeCollector(provider)
       const l2BaseCollector = await getL2BaseFeeCollector(provider)
       const l2SurplusCollector = await getL1SurplusFeeCollector(provider)
@@ -22,7 +29,9 @@ export function getFeeCollectorsCommand(program: Command) {
       str += `\nL2 Base Collector: ${l2BaseCollector}`
       str += `\nL2 Surplus Collector: ${l2SurplusCollector}`
       str += `\nBatch Posters -> L1 Base Collector:\n${tab}`
-      str += posters.map((poster, i) => `${poster} -> ${l1BaseCollectors[i]}`).join(`\n${tab}`)
+      str += posters
+        .map((poster, i) => `${poster} -> ${l1BaseCollectors[i]}`)
+        .join(`\n${tab}`)
       log.info(str)
     })
 }
