d73bf149b732f4c08c2c7f1a598467c8c9b4af93
Henry <11198460+godzillaba@users.noreply.github.com>
2025-01-07 15:36:29 -0500

    parallel

--- src/commands/fee-collectors.ts
+++ src/commands/fee-collectors.ts
@@ -1,11 +1,12 @@
 import { Command } from '@commander-js/extra-typings'
 import { log } from '../utils/logger'
-import { providers } from 'ethers'
+import { BigNumber, providers } from 'ethers'
 import {
   getBatchPosters,
   getL1BaseFeeCollector,
   getL1SurplusFeeCollector,
   getL2BaseFeeCollector,
+  getL2SurplusFeeCollector,
 } from '../utils/arb-precompiles'
 import {
   getRewardDistributorRecipients,
@@ -21,32 +22,71 @@ export function getFeeCollectorsCommand(program: Command) {
     .action(async options => {
       const provider = new providers.JsonRpcProvider(options.rpc)
       const tab = '  '
-      const posters = await getBatchPosters(provider)
-      const l1BaseCollectors = await Promise.all(
-        posters.map(poster => getL1BaseFeeCollector(provider, poster))
-      )
-      const l1SurplusCollector = await getL1SurplusFeeCollector(provider)
-      const l2BaseCollector = await getL2BaseFeeCollector(provider)
-      const l2SurplusCollector = await getL1SurplusFeeCollector(provider)
+
+      const [posters, l1Surplus, l2Base, l2Surplus] = await Promise.all([
+        (async () => {
+          const posters = await getBatchPosters(provider)
+          const l1BaseCollectorsAndRdInfo = await Promise.all(
+            posters.map(async poster => {
+              const collector = await getL1BaseFeeCollector(provider, poster)
+              return {
+                collector,
+                rdData: await getRdData(collector),
+              }
+            })
+          )
+          return {
+            posters,
+            collectors: l1BaseCollectorsAndRdInfo.map(x => x.collector),
+            rdDatas: l1BaseCollectorsAndRdInfo.map(x => x.rdData),
+          }
+        })(),
+        (async () => {
+          const collector = await getL1SurplusFeeCollector(provider)
+          return {
+            collector,
+            rdData: await getRdData(collector),
+          }
+        })(),
+        (async () => {
+          const collector = await getL2BaseFeeCollector(provider)
+          return {
+            collector,
+            rdData: await getRdData(collector),
+          }
+        })(),
+        (async () => {
+          const collector = await getL2SurplusFeeCollector(provider)
+          return {
+            collector,
+            rdData: await getRdData(collector),
+          }
+        })(),
+      ])
+      async function getRdData(collector: string) {
+        return options.distributor &&
+          (await isRewardDistributor(collector, provider))
+          ? await getRewardDistributorRecipients(collector, provider)
+          : null
+      }
+      function fmtRdData(
+        rdData: { recipients: string[]; weights: BigNumber[] } | null,
+        prefix: string
+      ) {
+        return rdData
+          ? `(RD):\n${rdData.recipients.map((r, i) => `${prefix}${r}: ${rdData.weights[i].toNumber() / 100}%`).join('\n')}`
+          : ''
+      }
 
       let str = ''
-      str += `L1 Surplus Collector: ${l1SurplusCollector} ${await extraRdInfo(l1SurplusCollector, tab)}`
-      str += `\nL2 Base Collector: ${l2BaseCollector} ${await extraRdInfo(l2BaseCollector, tab)}`
-      str += `\nL2 Surplus Collector: ${l2SurplusCollector} ${await extraRdInfo(l2SurplusCollector, tab)}`
+      str += `L2 Base Collector: ${l2Base.collector} ${fmtRdData(l2Base.rdData, tab)}`
+      str += `\nL2 Surplus Collector: ${l2Surplus.collector} ${fmtRdData(l2Surplus.rdData, tab)}`
+      str += `\nL1 Surplus Collector: ${l1Surplus.collector} ${fmtRdData(l1Surplus.rdData, tab)}`
       str += `\nBatch Posters -> L1 Base Collector:`
-      for (let i = 0; i < posters.length; i++) {
-        str += `\n${tab}${posters[i]} -> ${l1BaseCollectors[i]} ${await extraRdInfo(l1SurplusCollector, tab + tab)}`
+      for (let i = 0; i < posters.posters.length; i++) {
+        str += `\n${tab}${posters.posters[i]} -> ${posters.collectors[i]} ${fmtRdData(posters.rdDatas[i], tab + tab)}`
       }
-      log.info(str)
 
-      async function extraRdInfo(addr: string, prefix: string) {
-        if (
-          !options.distributor ||
-          !(await isRewardDistributor(addr, provider))
-        )
-          return ''
-        const d = await getRewardDistributorRecipients(addr, provider)
-        return `(RD):\n${d.recipients.map((r, i) => `${prefix}${r}: ${d.weights[i].toNumber() / 100}%`).join('\n')}`
-      }
+      log.info(str)
     })
 }
